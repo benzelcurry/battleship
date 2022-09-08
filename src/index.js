aedaa9d94cffd8848763060b940aed4319430916
benzelcurry <benawhiting@gmail.com>
2022-09-08 13:55:54 -0700

    Add task list for development

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 // Main module where everything comes together
 
+// Steps to complete before project completion:
+// 1. ADD BUTTON TO ALLOW USERS TO PLACE SHIPS VERTICALLY
+// 1.1 ADD OPTIONS TO PLACEMENT FUNCTIONS TO ENSURE SHIPS AREN'T OVERLAPPING/GOING OFF EDGES WHEN VERTICAL
+// 2. MAKE SHIPS PLACE RANDOMLY ON COMPUTER'S BOARD
+// 3. ADD A TURN-BASED SYSTEM
+// 4. GIVE COMPUTER A BASIC AI
+// 5. ANNOUNCE WINNER ONCE GAME IS OVER
+
 import './style.css';
 import Gameboard from './gameboard.js';
 import drawBoard from './interface';
